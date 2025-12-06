e61dd1e62bb1f24aa0513d62720d9e2f31607f69
Phantasmysteria <wallacechan58185@gmail.com>
2025-12-06 14:31:10 +0800

    feat: add aocutils list lines with space

--- aocutils.py
+++ aocutils.py
@@ -7,11 +7,16 @@ def listLines(day: str, part: str) -> List[str]:
     with open(f"inputD{day}Ex.txt" if part == "ex" else f"inputD{day}P{part}.txt", "r") as f:
         return [item.rstrip() for item in f.readlines()]
 
+# Quick way to convert input file into list of lines (without removing end spaces)
+def listLinesWithSpaces(day: str, part: str) -> List[str]:
+    with open(f"inputD{day}Ex.txt" if part == "ex" else f"inputD{day}P{part}.txt", "r") as f:
+        return [item[:-1] if item[-1] == '\n' else item for item in f.readlines()]
+
 # Quick way to convert input file into list of list of lines separated by delimiter
 def listLinesWithGroup(day: str, part: str, groupSep: str) -> List[str]:
     with open(f"inputD{day}Ex.txt" if part == "ex" else f"inputD{day}P{part}.txt", "r") as f:
         return [list(group) for key, group in itertools.groupby([item.rstrip() for item in f.readlines()], lambda x: x == groupSep) if not key]
-    
+
 # Output pretty print to output.txt
 def outputToTextFile(data: Any) -> None:
     with open("output.txt", "w") as f:
@@ -27,7 +32,9 @@ def sliceView(data: dict, start: int, end: int) -> dict:
             ret[k] = v
     return ret
 
-
+class TwoDGrid:
+    def __init__(self):
+        pass
 
 if __name__ == '__main__':
     print("Why are you running this file?")
