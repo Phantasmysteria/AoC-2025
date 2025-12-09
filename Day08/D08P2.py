64e82a233f533cb9db042144a34ed903c0780c0b
Phantasmysteria <wallacechan58185@gmail.com>
2025-12-09 20:29:50 +0800

    feat: add day 08

---
+++ Day08/D08P2.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.append('..')
+import aocutils
+import numpy as np
+
+data = {}
+data["00-raw"] = aocutils.listLines(day="08", part="2")
+dataLength = len(data["00-raw"])
+
+# it's numpy time
+data["01-parsed"] = np.array([line.split(',') for line in data["00-raw"]]).astype(int)
+
+# (N, 3) => (N, N)
+data["02-squared"] = np.sum(data["01-parsed"]**2, axis=1)
+data["03-allPairsDist"] = np.triu(data["02-squared"] - 2*np.matmul(data["01-parsed"], data["01-parsed"].T) + data["02-squared"][:, np.newaxis]).flatten()
+
+data["04-pairs"] = [(*divmod(x, dataLength), data["03-allPairsDist"][x]) for x in np.argsort(data["03-allPairsDist"]) if data["03-allPairsDist"][x] > 0]
+
+data["05-boxes"] = aocutils.UnionFindSize(dataLength)
+
+step = 0
+while True:
+    data["05-boxes"].union(data["04-pairs"][step][0], data["04-pairs"][step][1])
+    if max(data["05-boxes"].sizes) == dataLength:
+        break
+    step += 1
+
+output = data["01-parsed"][data["04-pairs"][step][0]][0] * data["01-parsed"][data["04-pairs"][step][1]][0]
+print(output)
+
+aocutils.outputToTextFile(data)
